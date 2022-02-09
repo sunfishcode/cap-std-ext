2739b0e4da97897a27c0e4d038ac8f46cf184e82
Dan Gohman <dev@sunfishcode.online>
2022-02-09 15:41:26 -0800

    Debugging.

--- tests/it/main.rs
+++ tests/it/main.rs
@@ -13,14 +13,20 @@ fn take_fd() -> Result<()> {
     let mut c = Command::new("/bin/bash");
     c.arg("-c");
     c.arg("wc -c <&5");
+    dbg!("hi");
     let (r, w) = rustix::io::pipe()?;
+    dbg!("hi");
     let r = Arc::new(r);
     let mut w = cap_std::fs::File::from_fd(w.into());
     c.take_fd_n(r.clone(), 5);
+    dbg!("hi");
     write!(w, "hello world")?;
+    dbg!("hi");
     drop(w);
     c.stdout(std::process::Stdio::piped());
+    dbg!("hi");
     let s = c.output()?;
+    dbg!("hi");
     assert!(s.status.success());
     assert_eq!(s.stdout.as_slice(), b"11\n");
     Ok(())
